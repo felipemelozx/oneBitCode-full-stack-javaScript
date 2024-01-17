e84442c9b18ab9d4502152763728ec026550e9ec
felipemelozx <felipemelozx@gmail.com>
2024-01-17 11:07:15 -0300

    ex-01 primeiro ex usando react

---
+++ mdulo-react/ex-01/src/App.tsx
@@ -0,0 +1,39 @@
+
+function App() {
+  return (
+    <main>
+      <div>
+        <img width="140px" src="public/react.png" alt="logo do react" />
+        <h1>React</h1>
+        <p>A biblioteca para interface de usuario web e nativo</p>
+        <button>Aprendendo React</button>
+        <button>Referencia da API</button>
+      </div>
+      <hr />
+      <div>
+        <h1>Crie interfaces de usuario de componentes</h1>
+        <p>React permite que voce construa interfaces de usuario a partir de pedacos individuais chamados componentes</p>
+      </div>
+      <hr />
+      <div>
+        <h1>Escreva componentes com codigo e marcacao</h1>
+        <p>Componentes React sao funcoes JavaScript. A sintaxe de marcacao e chamado JSX. E uma extencao da sintaxe JavaScript popularizada pelo React</p>
+      </div>
+      <hr />
+      <div>
+        <h1>Proximos passos</h1>
+        <ul>
+          <li>Uso de dados dinamicos no JSX </li>
+          <li>Criacao de novos componentes</li>
+          <li>Estilizacao de componentes </li>
+          <li>Reutilizacao de componentes</li>
+          <li>Uso de props e children</li>
+          <li>Uso de eventos do JavaScript</li>
+        </ul>
+      </div>
+    </main>
+  )
+
+}
+
+export default App
