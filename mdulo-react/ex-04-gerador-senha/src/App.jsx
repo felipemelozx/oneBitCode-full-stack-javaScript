e72063300e6e634cf544a4e1cab03cfe000a8af9
felipemelozx <felipemelozx@gmail.com>
2024-01-19 12:05:01 -0300

    formulario com react

--- mdulo-react/ex-04-gerador-senha/src/App.jsx
+++ mdulo-react/ex-04-gerador-senha/src/App.jsx
@@ -4,13 +4,12 @@ import './App.css'
 function App() {
   const [senha, setSenha] = useState("")
   const [copyText, setCopyText] = useState("copiar")
-
+  const [tamanhoSenha, setTamanhoSenha] = useState(10)
   function generater() {
     const caracters = "'1234567890-=!@#$%¨&*()_+qwertyuiop[asdfghjklç~]zxcvbnm,.;/QWERTYUIOP{ASDFGHJKLÇ^}ZXCVBNM<>:?"
-    const length = 12
     let novaSenha = ""
 
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < tamanhoSenha; i++) {
       const position = Math.floor(Math.random() * caracters.length)
       novaSenha += caracters[position]
     }
@@ -31,7 +30,16 @@ function App() {
   return (
     <div className="app">
       <h1>Gerador de Senha</h1>
-      <button onClick={generater}>Gerar</button>
+      <div>
+        <label htmlFor="tamanhoSenha">Tamanho: </label>
+        <input 
+        type="number" 
+        id='tamanhoSenha'
+        min={7}
+        value={tamanhoSenha}
+        onChange={(ev)=>{ setTamanhoSenha(ev.target.value)}}/>
+      </div>
+      <button onClick={generater}>Gerar senha com tamnho {tamanhoSenha}</button>
       <button onClick={copy}>{copyText}</button>
       <h2>{senha}</h2>
     </div>
