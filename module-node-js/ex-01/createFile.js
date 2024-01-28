a56bd881d10d52feb4689063d5b77066f2725efd
felipemelozx <felipemelozx@gmail.com>
2024-01-28 13:02:01 -0300

    create function createFile

---
+++ module-node-js/ex-01/createFile.js
@@ -0,0 +1,13 @@
+import fs from "node:fs"
+
+export default function createFile (conteudo){
+  fs.writeFile("meuarquivo.txt",conteudo, (err)=>{
+    if(err){
+      console.log("erro ao criar arquivo")
+      return
+    }
+    console.log("Arquivo criado com sucesso.")
+  })
+}
+
+createFile("ola,mundo")
