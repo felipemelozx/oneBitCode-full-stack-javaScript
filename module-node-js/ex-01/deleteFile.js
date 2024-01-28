f37aff51e5e82ddb499a1c922de0a0c2500da8bd
felipemelozx <felipemelozx@gmail.com>
2024-01-28 13:27:34 -0300

    create function delete file

---
+++ module-node-js/ex-01/deleteFile.js
@@ -0,0 +1,13 @@
+import fs from 'node:fs'
+
+export default function deleteFile(name){
+  fs.unlink(name, (err)=>{
+    if(err){
+      console.log('Erro ao deletar arquivo.')
+    }
+    console.log('Arquivo deletado com sucesso.')
+
+  })
+}
+
+deleteFile("meuarquivo.txt")
