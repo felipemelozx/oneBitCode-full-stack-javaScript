aff789f37fe79fb5f65ba8a4cad688e458ddf5f1
felipemelozx <felipemelozx@gmail.com>
2024-01-30 10:58:48 -0300

    abb uptime

--- module-node-js/ex-02/detalhesSistema.js
+++ module-node-js/ex-02/detalhesSistema.js
@@ -1,6 +1,6 @@
 const os = require("node:os")
 const path = require("node:path")
-
+const fs = require("node:fs")
 const systemPlatformMap = {
   "win32": "Windows",
   "linux": "Linux",
@@ -8,28 +8,35 @@ const systemPlatformMap = {
   "freebsd": "FreeBSD"
 }
 function detalhesPC() {
-  const modeloProcessador = os.cpus() 
+  const sistema = systemPlatformMap[os.platform()]
+  const modeloProcessador = os.cpus()[0].model
   const arquitetura = os.arch()
-  const sistema = os.platform()
   const tempoAtivo = os.uptime()
   const usoMemoriaRam = os.freemem()
-  switch (sistema) {
-    case 'win32':
-      console.log("seu sistema operacional e windows")
-      console.log(modeloProcessador[0].model)
-      console.log(arquitetura)
-      console.log(tempoAtivo)
-      console.log((usoMemoriaRam / (1024 * 1024 * 1024)).toFixed(2))
-      break;
-    case 'linux':
-      console.log("seu sistema operacional e Linux")
-      break;
-    case 'darwin':
-      console.log("seu sistema operacional e MacOs")
-      break;
-    case 'freebsd':
-      console.log("seu sistema operacional e FreeBSD")
-      break;
-  }
+
+  const uptimeDays = Math.floor(os.uptime() / 60 / 60 / 24)
+  const uptimeDaysInSeconds = uptimeDays * 24 * 60 * 60
+
+  const uptimeHours = Math.floor((os.uptime() - uptimeDaysInSeconds) / 60 / 60)
+  const uptimeHoursInSeconds = uptimeHours * 60 * 60
+
+  const uptimeMins = Math.floor((os.uptime() - uptimeDaysInSeconds - uptimeHoursInSeconds) / 60)
+  const uptimeMinsInSeconds = uptimeMins * 60
+
+  const uptimeSecs = Math.floor(os.uptime() - uptimeDaysInSeconds - uptimeHoursInSeconds - uptimeMinsInSeconds)
+
+  const uptime = `${uptimeDays}:${uptimeHours}:${uptimeMins}:${uptimeSecs}`
+
+
+  console.clear()
+  console.log('Detalhes do sistema operacional.')
+  console.log('O seu sistema operacional e: ' + sistema)
+  console.log('A sua arquitetura e: ' + arquitetura)
+  console.log('O modelo do processador: ' + modeloProcessador)
+  console.log('Uso de memoria ram: ' + usoMemoriaRam)
+  console.log('Tempo ativo: ' + uptime +' Dias')
+
 }
-detalhesPC()
+setInterval(() => {
+  detalhesPC()
+}, 1000)
