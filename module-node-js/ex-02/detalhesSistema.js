c35edad0827f6564cfea4d8f8b26127c9ed71e6e
felipemelozx <felipemelozx@gmail.com>
2024-01-30 11:51:17 -0300

    finished ex-02

--- module-node-js/ex-02/detalhesSistema.js
+++ module-node-js/ex-02/detalhesSistema.js
@@ -11,32 +11,45 @@ function detalhesPC() {
   const sistema = systemPlatformMap[os.platform()]
   const modeloProcessador = os.cpus()[0].model
   const arquitetura = os.arch()
-  const tempoAtivo = os.uptime()
-  const usoMemoriaRam = os.freemem()
+
+  const memoriaLivre = os.freemem() / 1024 / 1024 / 1024
+  const ramTotal = os.totalmem() / 1024 / 1024 / 1024
+  const usoMemoriaRam = ramTotal - memoriaLivre
+  const ramUsagePercent = (usoMemoriaRam / ramTotal) * 100
 
   const uptimeDays = Math.floor(os.uptime() / 60 / 60 / 24)
   const uptimeDaysInSeconds = uptimeDays * 24 * 60 * 60
-
   const uptimeHours = Math.floor((os.uptime() - uptimeDaysInSeconds) / 60 / 60)
   const uptimeHoursInSeconds = uptimeHours * 60 * 60
-
   const uptimeMins = Math.floor((os.uptime() - uptimeDaysInSeconds - uptimeHoursInSeconds) / 60)
   const uptimeMinsInSeconds = uptimeMins * 60
-
   const uptimeSecs = Math.floor(os.uptime() - uptimeDaysInSeconds - uptimeHoursInSeconds - uptimeMinsInSeconds)
-
   const uptime = `${uptimeDays}:${uptimeHours}:${uptimeMins}:${uptimeSecs}`
 
+  return { uptime, sistema, modeloProcessador, arquitetura, usoMemoriaRam, ramTotal, ramUsagePercent }
+}
+function saveLog(info) {
+  const log = 'Detalhes do sistema operacional. | O seu sistema operacional e: ' + info.sistema + ' | A sua arquitetura e: ' + info.arquitetura + ' | O modelo do processador: ' + info.modeloProcessador
+  ' | Uso de memoria ram: ' + info.usoMemoriaRam.toFixed(2) + ' GB' + ' / ' + info.ramTotal.toFixed(2) + ' GB ' + info.ramUsagePercent.toFixed(2) + '%'
+  'Tempo ativo: ' + info.uptime + ' Dias'
 
+  const caminhoLog = path.join("/", "log")
+  if (!fs.existsSync(caminhoLog)) {
+    fs.mkdirSync(caminhoLog)
+  }
+  const logFilePath = path.join(caminhoLog, "log.txt")
+  fs.appendFileSync(logFilePath, log)
+}
+function printLog(info) {
   console.clear()
   console.log('Detalhes do sistema operacional.')
-  console.log('O seu sistema operacional e: ' + sistema)
-  console.log('A sua arquitetura e: ' + arquitetura)
-  console.log('O modelo do processador: ' + modeloProcessador)
-  console.log('Uso de memoria ram: ' + usoMemoriaRam)
-  console.log('Tempo ativo: ' + uptime +' Dias')
-
+  console.log('O seu sistema operacional e: ' + info.sistema)
+  console.log('A sua arquitetura e: ' + info.arquitetura)
+  console.log('O modelo do processador: ' + info.modeloProcessador)
+  console.log('Uso de memoria ram: ' + info.usoMemoriaRam.toFixed(2) + ' GB' + ' / ' + info.ramTotal.toFixed(2) + ' GB ' + info.ramUsagePercent.toFixed(2) + '%')
+  console.log('Tempo ativo: ' + info.uptime + ' Dias')
 }
 setInterval(() => {
-  detalhesPC()
+printLog(detalhesPC())
+saveLog(detalhesPC())
 }, 1000)
