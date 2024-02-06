63c0c6c4f719060d4207a4cb7ae17dcef84e01b1
felipemelozx <felipemelozx@gmail.com>
2024-02-06 18:33:40 -0300

    Configurando o painel Adminj

--- onebitflix/src/server.ts
+++ onebitflix/src/server.ts
@@ -1,13 +1,17 @@
 import express from "express"
 import { sequelize } from "./database"
+import { adminJs, adminJsRouter } from "./adiminjs"
 
 const app = express()
+
+app.use(express.static('public'))
+app.use(adminJs.options.rootPath, adminJsRouter)
 
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, async () => {
   await sequelize.authenticate().then(() => {
-    console.log('deu certo')
+    console.log('Conectado ao BD.')
   }).catch((err) => {
     console.log("algo deu errado" + err)
   })
